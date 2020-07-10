2ac79d28b548431ae1303c32fa70c4a7900f658a
James Conway <jimmycon1232@gmail.com>
2020-07-10 17:33:27 -0500

    fix: Removed edit-google-spreadsheets and code trying to access it due to security vulnerabilities. Will look into different options later.

--- src/actions/submitted.js
+++ src/actions/submitted.js
@@ -1,4 +1,3 @@
-var Spreadsheet = require('edit-google-spreadsheet');
 const googleSheetCred = require('../../googleAppsScriptCredentials.json');
 
 async function submitted ({ ack, body, view, context, client }) {
@@ -46,24 +45,6 @@ async function submitted ({ ack, body, view, context, client }) {
         const usersRealName = userInfo.real_name;
 
         console.log('here!');
-
-        Spreadsheet.load({
-            debug: true,
-            spreadsheetName : 'Office Reopening COVID-19 Data',
-            worksheetName: 'Sheet1',
-
-            oauth2: {
-                client_id: googleSheetCred.web.client_id,
-                client_secret: googleSheetCred.web.client_secret,
-                refresh_token: 'hmm',
-            }
-        }, function sheetReady(err, spreadsheet) {
-            spreadsheet.add({ 3: { 5: 'hello!' } });
-            spreadsheet.send(function(err) {
-                if(err) { throw err; }
-                console.log('Updated cell at row 3 column 5 to hello!');
-            })
-        });
 
         await ack();
     } catch(error) {
